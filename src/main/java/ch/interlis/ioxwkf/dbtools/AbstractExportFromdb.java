4a94fbdc4b988e638e4eb1af1cc4f09abd0ba934
Renato Hiltbrunner <rh@eisenhutinformatik.ch>
2017-11-28 11:36:26 +0100

    changed way to export data with 2 selects to db, because there were to many selects (claeis/iox-wkf#11)

--- src/main/java/ch/interlis/ioxwkf/dbtools/AbstractExportFromdb.java
+++ src/main/java/ch/interlis/ioxwkf/dbtools/AbstractExportFromdb.java
@@ -2,12 +2,11 @@ package ch.interlis.ioxwkf.dbtools;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Types;
-import java.util.ArrayList;
+import java.util.List;
 import ch.ehi.basics.logging.EhiLogger;
 import ch.ehi.basics.settings.Settings;
 import ch.ehi.ili2db.converter.ConverterException;
@@ -20,24 +19,9 @@ import ch.interlis.iox_j.EndBasketEvent;
 import ch.interlis.iox_j.EndTransferEvent;
 
 public abstract class AbstractExportFromdb {
-	
-	/** list of attribute descriptors.
-	 */
-	private ArrayList<AttributeDescriptor> attrs=new ArrayList<AttributeDescriptor>();
-	
-	/** number of iomObjects which were created.
-	 */
-	private int objectCount=0;
-	
 	private IoxFactoryCollection factory;
-	
-	/** postgis column converter to convert to db and back.
-	 */
+	private int objectCount=0;
 	private PostgisColumnConverter pgConverter=new PostgisColumnConverter();
-	
-	/** srsCode: set default value.
-	 */
-	private Integer srsCode=IoxWkfConfig.SETTING_SRSCODE_DEFAULT;
 	
 	/** default model content.
 	 */
@@ -59,9 +43,9 @@ public abstract class AbstractExportFromdb {
 	 * @param file to write to.
 	 * @param connection to db.
 	 * @param config to set by user.
-	 * @throws IoxException
+	 * @throws IoxException 
 	 */
-	public void exportData(File file,Connection db,Settings config) throws IoxException {
+	protected void exportData(File file,Connection db,Settings config) throws IoxException {
 		/** data base connection has not to be null.
 		 */
 		if(db==null) {
@@ -90,62 +74,16 @@ public abstract class AbstractExportFromdb {
 
 		/** create selection to get information about attributes of target data base table.
 		 */
-		ResultSet dbTable=null;
+		List<AttributeDescriptor> attributes=null;
 		try {
-			dbTable=openTableInDb(definedSchemaName, definedTableName, db);
-			if(definedSchemaName!=null) {
-				EhiLogger.logState("db table <"+definedTableName+"> inside db schema <"+definedSchemaName+">: exist.");
-			}else {
-				EhiLogger.logState("db table <"+definedTableName+"> inside default db schema: exist.");
-			}
+			attributes=AttributeDescriptor.getAttributeDescriptors(definedSchemaName, definedTableName, db);
+			AttributeDescriptor.addGeomDataToAttributeDescriptors(definedSchemaName, definedTableName, attributes, db);
 		}catch(Exception e) {
 			if(definedSchemaName!=null) {
 				throw new IoxException("db table <"+definedTableName+"> inside db schema <"+definedSchemaName+">: not found.",e);
 			}else{
 				throw new IoxException("db table "+definedTableName+" inside default db schema: not found.",e);
 			}
-		}
-		
-		/** set attribute data to target attribute, to create wrapper selection.
-		 */
-		ResultSetMetaData metadataDbTable;
-		try {
-			metadataDbTable = dbTable.getMetaData();
-		} catch (SQLException e1) {
-			throw new IoxException(e1);
-		}
-		try {
-			for(int k=1;k<metadataDbTable.getColumnCount()+1;k++) {
-				// columnName
-				String columnName=metadataDbTable.getColumnName(k);
-				// columnType
-				int columnType=metadataDbTable.getColumnType(k);
-				// columnTypeName
-				String columnTypeName=metadataDbTable.getColumnTypeName(k);
-				
-				// set PG Attribute Object data.
-				AttributeDescriptor attr=new AttributeDescriptor();
-				attr.setAttributeName(columnName);
-				attr.setAttributeType(columnType);
-				attr.setAttributeTypeName(columnTypeName);
-				
-				// put attribute to attribute descriptor list.
-				attrs.add(attr);
-			}
-		} catch (SQLException e) {
-			throw new IoxException(e);
-		}
-		
-		/** create selection for appropriate datatypes.
-		 *  geometry datatypes are wrapped from pg to ili.
-		 */
-		ResultSet pg2IliConvertedTable=null;
-		try {
-			pg2IliConvertedTable=openPgToIliConvertedTableInDb(definedSchemaName, definedTableName, db);
-		}catch(IoxException e) {
-			throw new IoxException(e);
-		} catch (SQLException e) {
-			throw new IoxException(e);
 		}
 		
 		/** The final IomObjects will be send in ioxObjectEvents and written as individual records to the given file.
@@ -159,26 +97,45 @@ public abstract class AbstractExportFromdb {
 		 */
 		IoxWriter writer=createWriter(file, config);
 		
-		// add attribute value, converted in appropriate type, list of attribute descriptors.
+		PreparedStatement ps=null;
+		ResultSet rs=null;
 		try {
-			while(pg2IliConvertedTable.next()) {
-				// create iomObjects
-				try {
-					iomObject=getRecordsAsIomObjects(definedSchemaName,definedTableName, MODELNAME, TOPICNAME, pg2IliConvertedTable, db);
-				} catch (IoxException e) {
-					throw new IoxException(e);
-				}
-				if(iomObject.getattrcount()==0) {
-					throw new IoxException("no data found to export to file.");
-				}
+			/** create selection for appropriate datatypes.
+			 *  geometry datatypes are wrapped from db to ili.
+			 */
+			String selectQuery = getSelectStatement(definedSchemaName, definedTableName, attributes, db);
+			ps = db.prepareStatement(selectQuery);
+			ps.clearParameters();
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				/** convert records to iomObject data types.
+				 */
+				iomObject=convertRecordToIomObject(definedSchemaName,definedTableName, MODELNAME, TOPICNAME, attributes, rs, db);
 				try {
 					writer.write(new ch.interlis.iox_j.ObjectEvent(iomObject));
-				}catch(Exception e) {
+				}catch(IoxException e) {
 					throw new IoxException("export of: <"+iomObject.getobjecttag()+"> to file: <"+file.getAbsolutePath()+"> failed.",e);
 				}
 			}
 		} catch (SQLException e) {
 			throw new IoxException(e);
+		}finally{
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					throw new IoxException(e);
+				}
+				rs=null;
+			}
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					throw new IoxException(e);
+				}
+				ps=null;
+			}
 		}
 		EhiLogger.logState("conversion of attributes: <successful>.");
 		writer.write(new EndBasketEvent());
@@ -194,52 +151,7 @@ public abstract class AbstractExportFromdb {
 		EhiLogger.logState("export: <successful>.");
 	}
 	
-	/** select table in database and get attribute data.
-	 * @param schemaName
-	 * @param tableName
-	 * @param db
-	 * @return resultset of db table.
-	 * @throws IoxException
-	 * @throws SQLException 
-	 */
-	private ResultSet openTableInDb(String schemaName, String tableName, Connection db) throws IoxException, SQLException {
-		StringBuffer queryBuild=new StringBuffer();
-		queryBuild.append("SELECT * FROM ");
-		if(schemaName!=null) {
-			queryBuild.append(schemaName+".");
-		}
-		queryBuild.append(tableName+";");
-		Statement stmt = db.createStatement();
-		ResultSet rs=stmt.executeQuery(queryBuild.toString());
-		return rs;
-	}
-	
-	/** get resultset of geometry columns.
-	 * @param schemaName
-	 * @param tableName
-	 * @param db
-	 * @return resultset of geometry columns.
-	 * @throws IoxException
-	 */
-	private ResultSet openGeometryColumnTableInDb(String schemaName, String tableName, String attrName, Connection db) throws IoxException {
-		ResultSet rs =null;
-		StringBuffer queryBuild=new StringBuffer();
-		queryBuild.append("SELECT * FROM geometry_columns WHERE ");
-		if(schemaName!=null) {
-			queryBuild.append("f_table_schema='"+schemaName+"' AND ");
-		}
-		queryBuild.append("f_table_name='"+tableName+"' "); 
-		queryBuild.append("AND f_geometry_column='"+attrName+"';");
-		try {
-			Statement stmt = db.createStatement();
-			rs=stmt.executeQuery(queryBuild.toString());
-		} catch (SQLException e) {
-			throw new IoxException(e);
-		}
-		return rs;
-	}
-	
-	/** create a selection and wrapp all geometries, xml and blob datatypes from pg to ili format.
+	/** create a selection and wrapp all geometries, xml and blob datatypes from db to ili format.
 	 * @param schemaName
 	 * @param tableName
 	 * @param attrsPool
@@ -249,32 +161,21 @@ public abstract class AbstractExportFromdb {
 	 * @throws SQLException
 	 * @throws ConverterException
 	 */
-	private ResultSet openPgToIliConvertedTableInDb(String schemaName, String tableName, Connection db) throws IoxException, SQLException {
-		// create selection.
+	private String getSelectStatement(String schemaName, String tableName, List<AttributeDescriptor> attrs, Connection db) throws SQLException {
 		StringBuilder selectionQueryBuild=new StringBuilder();
 		String comma="";
 		selectionQueryBuild.append("SELECT ");
-		// attribute data type conversion.
+		/** convert each attribute to db valid data type.
+		 */
 		for(AttributeDescriptor attr:attrs) {
-			// attrName
 			String attrName=attr.getAttributeName();
-			// dataType
 			Integer datatype=attr.getAttributeType();
-			// dataTypeName
 			String geoColumnTypeGeom=attr.getAttributeTypeName();
-			
+			String geoColumnTypeName=attr.getGeomColumnTypeName();
 			selectionQueryBuild.append(comma);
 			if(datatype.equals(Types.OTHER)) {
 				// dataType is an object.
 				if(geoColumnTypeGeom!=null && geoColumnTypeGeom.equals(AttributeDescriptor.SET_GEOMETRY)) {
-					// get geometry dataType information
-					ResultSet geomColumnTableInDb=openGeometryColumnTableInDb(schemaName, tableName, attrName, db);
-					String geoColumnTypeName=null;
-					while(geomColumnTableInDb.next()) {
-						geoColumnTypeName=geomColumnTableInDb.getString(AttributeDescriptor.SET_TYPE);
-						// srId
-						srsCode=geomColumnTableInDb.getInt(AttributeDescriptor.SET_SRID);
-					}
 					// the object is a geometry.
 					if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_POINT)) {
 						selectionQueryBuild.append(pgConverter.getSelectValueWrapperCoord(attrName));
@@ -294,7 +195,6 @@ public abstract class AbstractExportFromdb {
 					selectionQueryBuild.append(attrName);
 				}
 			}else {
-				// dataType is type of jdbc.
 				selectionQueryBuild.append(attrName);
 			}
 			comma=",";
@@ -304,10 +204,7 @@ public abstract class AbstractExportFromdb {
 			selectionQueryBuild.append(schemaName+".");
 		}
 		selectionQueryBuild.append(tableName+";");
-		// end of selection query, execute created query.
-		Statement stmt=db.createStatement();
-		ResultSet rs=stmt.executeQuery(selectionQueryBuild.toString());
-		return rs;
+		return selectionQueryBuild.toString();
 	}
 	
 	/** get iomObject with all attributes in appropriate datatype.
@@ -317,43 +214,41 @@ public abstract class AbstractExportFromdb {
 	 * @param modelName
 	 * @param topicName
 	 * @return complete iomObject
-	 * @throws IoxException
 	 * @throws SQLException
+	 * @throws IoxException 
 	 * @throws ConverterException
 	 */
-	private IomObject getRecordsAsIomObjects(String definedSchemaName,String definedTableName, String modelName, String topicName, ResultSet pg2IliConvertedTable, Connection db) throws IoxException, SQLException {
+	private IomObject convertRecordToIomObject(String definedSchemaName,String definedTableName, String modelName, String topicName, List<AttributeDescriptor> attrs, ResultSet rs, Connection db) throws IoxException, SQLException {
 		IomObject geoIomObj=null;
-		// create iomObject to add attributes or objects in.
-		IomObject iomObj=createIomObject(modelName+"."+topicName+"."+definedTableName);
+		// create iomObject to put attributes or objects inside.
+		IomObject iomObj;
+		try {
+			iomObj = createIomObject(modelName+"."+topicName+"."+definedTableName);
+		} catch (IoxException e1) {
+			throw new IoxException(e1);
+		}
 		String geoColumnTypeName=null;
+		int position=0;
 		for(AttributeDescriptor attr:attrs) {
-			// data of attribute
+			position+=1;
 			String attrName=attr.getAttributeName();
 			String dataTypeName=attr.getAttributeTypeName();
 			Integer dataType=attr.getAttributeType();
-			// dataTypeName
-			String geoColumnTypeGeom=attr.getAttributeTypeName();
-			Integer coordDimension=0;
-			// attrValue
-			Object attrObj=null;
-			String attrValue=null;
-			if(dataTypeName.equals(AttributeDescriptor.SET_GEOMETRY)) {
-				attrObj=pg2IliConvertedTable.getObject("st_asewkb");
-			}else {
-				attrValue=pg2IliConvertedTable.getString(attrName);
+			String attrValue;
+			try {
+				attrValue = rs.getString(position);
+			} catch (SQLException e1) {
+				throw new IoxException(e1);
 			}
 			boolean is3D=false;
 			try {
-				// get attribute value in appropriate data type.
+				/** get attribute value in appropriate data type.
+				 */
 				if(dataType.equals(Types.OTHER)) {
-					if(geoColumnTypeGeom!=null && geoColumnTypeGeom.equals(AttributeDescriptor.SET_GEOMETRY)) {
-						// add geometry dataType information to map of PG attribute objects.
-						ResultSet geomColumnTableInDb=openGeometryColumnTableInDb(definedSchemaName, definedTableName, attrName, db);
-						while(geomColumnTableInDb.next()) {
-							geoColumnTypeName=geomColumnTableInDb.getString(AttributeDescriptor.SET_TYPE);
-							coordDimension=geomColumnTableInDb.getInt(AttributeDescriptor.SET_DIMENSION);
-							srsCode=geomColumnTableInDb.getInt(AttributeDescriptor.SET_SRID);
-						}
+					if(dataTypeName!=null && dataTypeName.equals(AttributeDescriptor.SET_GEOMETRY)) {
+						geoColumnTypeName=attr.getGeomColumnTypeName();
+						int coordDimension=attr.getCoordDimension();
+						String srsCode=String.valueOf(attr.getSrId());
 						if(coordDimension==3) {
 							is3D=true;
 						}else {
@@ -361,27 +256,27 @@ public abstract class AbstractExportFromdb {
 						}
 						// point
 						if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_POINT)) {
-							geoIomObj=pgConverter.toIomCoord(attrObj, srsCode.toString(), is3D);
+							geoIomObj=pgConverter.toIomCoord(rs.getObject(2), srsCode, is3D);
 							iomObj.addattrobj(attrName, geoIomObj);
 						// multipoint
 						}else if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_MULTIPOINT)) {
-							geoIomObj=pgConverter.toIomMultiCoord(attrObj, srsCode.toString(), is3D);
+							geoIomObj=pgConverter.toIomMultiCoord(rs.getObject(2), srsCode, is3D);
 							iomObj.addattrobj(attrName, geoIomObj);
 						// line
 						}else if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_LINESTRING)) {
-							geoIomObj=pgConverter.toIomPolyline(attrObj, srsCode.toString(), is3D);
+							geoIomObj=pgConverter.toIomPolyline(rs.getObject(2), srsCode, is3D);
 							iomObj.addattrobj(attrName, geoIomObj);
 						// multiline
 						}else if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_MULTILINESTRING)) {
-							geoIomObj=pgConverter.toIomMultiPolyline(attrObj, srsCode.toString(), is3D);
+							geoIomObj=pgConverter.toIomMultiPolyline(rs.getObject(2), srsCode, is3D);
 							iomObj.addattrobj(attrName, geoIomObj);
 						// polygon
 						}else if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_POLYGON)) {
-							geoIomObj=pgConverter.toIomSurface(attrObj, srsCode.toString(), is3D);
+							geoIomObj=pgConverter.toIomSurface(rs.getObject(2), srsCode, is3D);
 							iomObj.addattrobj(attrName, geoIomObj);
 						// multipolygon
 						}else if(geoColumnTypeName.equals(AttributeDescriptor.SET_GEOMETRY_MULTIPOLYGON)) {
-							geoIomObj=pgConverter.toIomMultiSurface(attrObj, srsCode.toString(), is3D);
+							geoIomObj=pgConverter.toIomMultiSurface(rs.getObject(2), srsCode, is3D);
 							iomObj.addattrobj(attrName, geoIomObj);
 						}
 					}else {
@@ -390,7 +285,7 @@ public abstract class AbstractExportFromdb {
 							iomObj.setattrvalue(attrName, attrValue);
 						// xml	
 						}else if(dataTypeName.equals(AttributeDescriptor.SET_XML)) {
-							iomObj.setattrvalue(attrName, pgConverter.toIomXml(attrObj));
+							iomObj.setattrvalue(attrName, pgConverter.toIomXml(rs.getObject(2)));
 						}
 					}
 				}else {
@@ -408,6 +303,8 @@ public abstract class AbstractExportFromdb {
 								iomObj.setattrvalue(attrName, "0");
 							}	
 						}
+					}else if(dataType.equals(Types.BIT)) {
+						iomObj.setattrvalue(attrName,attrValue);
 					}else if(dataType.equals(Types.BLOB)) {
 					    iomObj.setattrvalue(attrName,pgConverter.toIomBlob(attrValue));
 					}else if(dataType.equals(Types.BINARY)) {
@@ -465,9 +362,9 @@ public abstract class AbstractExportFromdb {
 					notConvertedAttr.append(dataTypeName);
 				}
 				notConvertedAttr.append(" failed by converting.");
-				throw new IoxException(notConvertedAttr.toString(),e);
+			}catch(SQLException e2) {
+				throw new SQLException(e2);
 			}
-			
 		}
 		return iomObj;
 	}
