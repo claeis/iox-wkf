6b457c4463eafaabead44a395ee689ef1e76959c
rhiltbrunner <rh@eisenhutinformatik.ch>
2018-01-05 12:55:27 +0100

    changed wrong comments and added comments to protected methods (claeis/iox-wkf#17)

--- src/main/java/ch/interlis/ioxwkf/dbtools/AttributeDescriptor.java
+++ src/main/java/ch/interlis/ioxwkf/dbtools/AttributeDescriptor.java
@@ -10,10 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import ch.interlis.iox.IoxException;
 
-/** the AttributeDescriptor class contains attribute types used within a feature. Attribute types are distinguished by 2 main types:
- * <p>
- * <li>A geometry type includes dimensions of coordinates, as well as the appropriate srid.</li>
- * <li>An attribute type is distinguished by 'primitive' data types and 'other' data types, which fall under the term: 'OTHER' like uuid or xml for example.</li>
+/** describes a list of dataTypes.
  */
 public class AttributeDescriptor {
 	private String dbColumnName=null;
@@ -25,60 +22,95 @@ public class AttributeDescriptor {
 	private Integer srId=null;
 	private Integer precision=null;
 	
-	// JDBC/DB column type name if java.sql.Types.OTHER
-	/** bool is an additional type name of JDBC/DB to identify the dataType boolean and is defined as char type.
-	 * <br>
-	 * valid boolean values are:
-	 * <li>'true'</li><li>'false'</li><li>'t'</li><li>'f'</li><li>'1'</li><li>'0'</li>
-	 * <br>
-	 * if boolean is true(t) and the JDBC/DB column type name is bool, then it is a boolean type. else could be char (1,0).
+	/** bool is an additional type name of boolean.
+	 * <p>
+	 * get type:<br>
+	 * <li>getDbColumnTypeName()</li>
 	 */
 	public final static String DBCOLUMN_TYPENAME_BOOL="bool";
-	/** xml fall under the term: 'OTHER'.<br>
-	 * the first character must be a letter or underscore, followed by
-	 * letters, numbers, periods, minus signs or underscores.<br>
-	 * no colons aloud here.<p>
-	 * @see <a href="www.w3.org/TR/REC-xml">www.w3.org/TR/REC-xml.</a>
+	
+	// JDBC/DB column type name if java.sql.Types.OTHER
+	/** xml is a name of JDBC/DB column type.
+	 * <p>
+	 * requirements:
+     * <li>has to be type of: 'java.sql.Types.OTHER'</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnTypeName()</li>
 	 */
 	public final static String DBCOLUMN_TYPENAME_XML="xml";
-	/** uuid's fall under the term: 'OTHER'.<br>
-	 * They are defined by 36 characters in format of: ISO 11578.<br>
-	 * The first character must be a letter or underscore, followed by
-	 * letters, numbers, periods, minus signs or underscores.<br>
-	 * No colons aloud here.<p>
-	 * @see <a href="www.w3.org/TR/REC-xml">www.w3.org/TR/REC-xml.</a>
+	/** xml is a name of JDBC/DB column type.<br>
+	 * it is defined by 36 characters in format of: ISO 11578.
+	 * <p>
+	 * requirements:
+     * <li>has to be type of: 'java.sql.Types.OTHER'</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnTypeName()</li>
 	 */
 	public final static String DBCOLUMN_TYPENAME_UUID="uuid";
-	/** geometry is the type name of JDBC/DB to identify the dataType as geometry.
+	/** geometry is the name of JDBC/DB column type to identify the dataType as geometry.
+	 * <p>
+	 * requirements:
+     * <li>has to be type of: 'java.sql.Types.OTHER'</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnTypeName()</li>
 	 */
 	public final static String DBCOLUMN_TYPENAME_GEOMETRY="geometry";
 	// geometry types
-	/** multiPolygon is a collection of Polygons.<br>
-	 * As per the OGC SFS specification, the Polygons in a MultiPolygon may not overlap,
-	 * and may only touch at single points. This allows the topological point-set
-	 * semantics to be well-defined.
+	/** multiPolygon is a collection of Polygons.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnGeomTypeName()</li>
 	 */
 	public final static String GEOMETRYTYPE_MULTIPOLYGON="MULTIPOLYGON";
-	/** represents a polygon with linear edges, which may include holes.
-	 * The outer boundary (shell) and inner boundaries (holes) of the polygon are represented by LinearRings.
-	 * The boundary rings of the polygon may have any orientation.
-	 * Polygons are closed and simple geometries by definition.
+	/** represents a polygon with linear edges, which includes shell and may includes holes.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnGeomTypeName()</li>
 	 */
 	public final static String GEOMETRYTYPE_POLYGON="POLYGON";
-	/** a MultiLineString is defined by one or more LineStrings,
-	 * referenced through lineString elements.
+	/** a MultiLineString is defined by one or more LineStrings.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnGeomTypeName()</li>
 	 */
 	public final static String GEOMETRYTYPE_MULTILINESTRING="MULTILINESTRING";
-	/** a sequence of line segments, each having a parameterization like the one LineSegment.
-	 * The class essentially combines a List of LineSegment into a single object.
+	/** lineString is a sequence of line segments.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnGeomTypeName()</li>
 	 */
 	public final static String GEOMETRYTYPE_LINESTRING="LINESTRING";
 	/** an aggregate class containing only instances of Point.
-	 * The association role element shall be the set of points
-	 * contained in this MultiPoint.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnGeomTypeName()</li>
 	 */
 	public final static String GEOMETRYTYPE_MULTIPOINT="MULTIPOINT";
-	/** a basic data type for a geometric object consisting of one point.
+	/** a geometric object consisting of one point.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+	 * get type:<br>
+	 * <li>getDbColumnGeomTypeName()</li>
 	 */
 	public final static String GEOMETRYTYPE_POINT="POINT";
 	
@@ -86,51 +118,145 @@ public class AttributeDescriptor {
 	private final static String GEOMCOLUMNS_COLUMN_SRID="srid";
 	private final static String GEOMCOLUMNS_COLUMN_DIMENSION="coord_dimension";
 	
+	/** get the name of column.
+	 * @return column name as String.
+	 */
 	protected String getDbColumnName() {
 		return dbColumnName;
 	}
+	/** set given name as column name in String.
+	 * @param attributeName
+	 */
 	protected void setDbColumnName(String attributeName) {
 		this.dbColumnName = attributeName;
 	}
+	/** get the attribute name of an iomObject.
+	 * @return object name in String.
+	 */
 	protected String getIomAttributeName() {
 		return iomAttributeName==null? dbColumnName : iomAttributeName;
 	}
+	/** set the given attribute name in type String.
+	 * @param attributeName
+	 */
 	protected void setIomAttributeName(String attributeName) {
 		this.iomAttributeName = attributeName;
 	}
+	/** get the type of JDBC/DB column.
+	 * <p>
+	 * requirements:
+     * <li>has to be a type of: java.sql.Types</li>
+     * <p>
+	 * @return typeNumber in integer.
+	 */
 	protected Integer getDbColumnType() {
 		return attributeType;
 	}
+	/** set the given attribute type.
+	 * <p>
+	 * requirements:
+     * <li>has to be a type of: java.sql.Types</li>
+     * <p>
+	 * @param attributeType
+	 */
 	protected void setDbColumnType(Integer attributeType) {
 		this.attributeType = attributeType;
 	}
+	/** get the name of DBColumn type.
+	 * <p>
+	 * requirements:
+     * <li>has to be type of: 'java.sql.Types.OTHER'</li>
+     * <p>
+	 * @return DBColumnTypeName in String.
+	 */
 	protected String getDbColumnTypeName() {
 		return attributeTypeName;
 	}
+	/** set the given DBColumn type name in String.
+	 * <p>
+	 * requirements:
+     * <li>has to be type of: 'java.sql.Types.OTHER'</li>
+     * <p>
+	 * @param attributeTypeName
+	 */
 	protected void setDbColumnTypeName(String attributeTypeName) {
 		this.attributeTypeName = attributeTypeName;
 	}
+	/** get the type of DBColumn geometry name.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+	 */
 	protected String getDbColumnGeomTypeName() {
 		return dbColumnGeomTypeName;
 	}
+	/** set the given geometry type name.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+     * @param dbColumnGeomTypeName in String.
+	 */
 	protected void setDbColumnGeomTypeName(String dbColumnGeomTypeName) {
 		this.dbColumnGeomTypeName = dbColumnGeomTypeName;
 	}
+	/** get the coordinate dimension.<br>
+	 * the dimension of the coordinates that define this Geometry, which must be the same as the coordinate dimension of the coordinate reference system for this Geometry.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+     * @return coordDimension in integer.
+	 */
 	protected Integer getCoordDimension() {
 		return coordDimension;
 	}
+	/** set the given coordinate dimension.<br>
+	 * the dimension of the coordinates that define this Geometry, which must be the same as the coordinate dimension of the coordinate reference system for this Geometry.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+     * @param coordDimension in Integer.
+	 */
 	protected void setCoordDimension(Integer coordDimension) {
 		this.coordDimension = coordDimension;
 	}
+	/** get the SRID.<br>
+	 * the srId is an integer value that uniquely identifies the Spatial Referencing System (SRS) within the database.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+     * @return srId in Integer.
+	 */
 	protected Integer getSrId() {
 		return srId;
 	}
+	/** set the given srId.<br>
+	 * the srId is an integer value that uniquely identifies the Spatial Referencing System (SRS) within the database.
+	 * <p>
+	 * requirements:
+     * <li>isGeometry() has to be true</li>
+     * <p>
+     * @param srId
+	 */
 	protected void setSrId(Integer srId) {
 		this.srId = srId;
 	}
+	/** get the precision in Integer.<br>
+	 * precision is the number of digits in the unscaled value.
+	 * <p>
+	 * @return precision.
+	 */
 	protected Integer getPrecision() {
 		return precision;
 	}
+	/** set the given bytePrecision in Integer.<br>
+	 * precision is the number of digits in the unscaled value.
+	 * <p>
+	 * @param precision
+	 */
 	protected void setPrecision(Integer precision) {
 		this.precision = precision;
 	}
@@ -211,7 +337,11 @@ public class AttributeDescriptor {
 		return attrs;
 	}
 	
-	/** check if current datatype is part of geometry.
+	/** check if this is a geometry type.
+     * <p>
+	 * get type:
+	 * <li>getDbColumnGeomTypeName()</li>
+	 * <p>
 	 * @return true if datatype is part of geometry, false if not.
 	 */
 	public boolean isGeometry() {
