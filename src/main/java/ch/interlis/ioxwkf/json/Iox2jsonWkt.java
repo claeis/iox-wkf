f46b69cb198f53488b5945dfffd928400532ea1b
claeis <ce@eisenhutinformatik.ch>
2021-05-18 13:44:36 +0200

    added JsonReader (#36)

--- src/main/java/ch/interlis/ioxwkf/json/Iox2jsonWkt.java
+++ src/main/java/ch/interlis/ioxwkf/json/Iox2jsonWkt.java
@@ -1,38 +1,17 @@
 package ch.interlis.ioxwkf.json;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.CoordinateList;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.WKTWriter;
 
 import ch.ehi.ili2db.json.Iox2json;
 import ch.interlis.ili2c.metamodel.AbstractSurfaceOrAreaType;
-import ch.interlis.ili2c.metamodel.AssociationDef;
-import ch.interlis.ili2c.metamodel.AttributeDef;
-import ch.interlis.ili2c.metamodel.CompositionType;
-import ch.interlis.ili2c.metamodel.CoordType;
-import ch.interlis.ili2c.metamodel.EnumerationType;
 import ch.interlis.ili2c.metamodel.LineType;
-import ch.interlis.ili2c.metamodel.MultiPolylineType;
-import ch.interlis.ili2c.metamodel.NumericType;
-import ch.interlis.ili2c.metamodel.ObjectType;
-import ch.interlis.ili2c.metamodel.PolylineType;
-import ch.interlis.ili2c.metamodel.RoleDef;
 import ch.interlis.ili2c.metamodel.TransferDescription;
-import ch.interlis.ili2c.metamodel.Type;
-import ch.interlis.ili2c.metamodel.Viewable;
-import ch.interlis.ili2c.metamodel.ViewableTransferElement;
-import ch.interlis.iom.IomConstants;
 import ch.interlis.iom.IomObject;
-import ch.interlis.iom_j.Iom_jObject;
 import ch.interlis.iox_j.jts.Iox2jts;
 import ch.interlis.iox_j.jts.Iox2jtsException;
 
